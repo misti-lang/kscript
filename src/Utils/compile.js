853731ed686ea28c5ca24a48f7132a84d81e5e35
Araozu <tbyuyfcjjll@gmail.com>
2020-06-10 12:36:34 -0500

    Agregada opcion para compilar archivo, y escribir el resultado en otro archivo.

--- src/Utils/compile.js
+++ src/Utils/compile.js
@@ -1,17 +1,44 @@
 const flujoPrincipal = require("../Inicio.bs").flujoPrincipal;
 const fs = require("fs");
 
-
-const compilar = ruta => {
+const compilar = (ruta, imprimirEnStdout=false) => {
     const data = fs.readFileSync(ruta, "utf-8");
 
     try {
         const jsResultado = flujoPrincipal(data);
-        console.log(jsResultado);
+
+        if (imprimirEnStdout) {
+            console.log(jsResultado);
+        }
+
+        fs.open(ruta + ".js", "w", (err, fd) => {
+            if (err) {
+                console.error(`Error al compilar el archivo ${ruta}:
+                               Se compiló el código, pero no se pudo crear el archivo resultado.`);
+                console.error(err);
+                process.exit(0);
+            }
+
+            fs.write(fd, jsResultado, (err) => {
+                if (err) {
+                    console.error(`Error al compilar el archivo ${ruta}:
+                               Se compiló el código, pero no se pudo escribir al archivo ${ruta}.js`);
+                    console.error(err);
+                    fs.closeSync(fd);
+                    process.exit(0);
+                }
+
+                fs.closeSync(fd);
+                console.log("Compilado sin problemas.");
+                process.exit(0);
+            });
+
+        });
+
     } catch (e) {
         console.error(e);
+        process.exit(0);
     }
-    process.exit(0);
 
 };
 
--- src/index.js
+++ src/index.js
@@ -3,9 +3,11 @@ const flujoREPL = require("./Utils/repl").flujoREPL;
 const compilar  = require("./Utils/compile").compilar;
 
 const strAyuda = `Uso:
-  --repl               Inicia el REPL
-  -c, --compile FILE   Compila el archivo FILE e imprime en stdout.
-  -h, --help           Muestra esta información`;
+  --repl               Inicia el REPL.
+  -c, --compile FILE   Compila el archivo FILE.
+  -cs FILE             Compila el archivo FILE y adicionalmente imprime en stdout.
+  -h, --help           Muestra esta información.
+  -v, --version        Imprime la versión del compilador.`;
 
 
 process.on("SIGINT", () => {
@@ -21,26 +23,35 @@ const main = function () {
     }
 
     const comando = process.argv[2];
+    let imprimirCompilacion = false;
     switch (comando) {
         case "-h":
         case "--help": {
             console.log(strAyuda);
             process.exit(0);
         }
+        case "-v":
+        case "--version": {
+            console.log("El compilador Misti, versión 0.0.13");
+            process.exit(0);
+        }
         case "--repl": {
             flujoREPL();
             break;
         }
+        case "-cs":
+            imprimirCompilacion = true;
         case "-c":
         case "--compile": {
             const ruta = process.argv[3];
             if (ruta) {
-                compilar(ruta);
+                compilar(ruta, imprimirCompilacion);
             } else {
                 console.log("Tienes que introducir una ruta a un archivo " +
                 "luego de usar " + comando + ".");
+                process.exit(0);
             }
-            process.exit(0);
+            break;
         }
         default: {
             console.log("Comando no reconocido. Usa -h para mas información.");
